600fceb03dcc5d93903c712f1d8dcaf717e1c31a
Jay-the-Creator <jaanov2013@gmail.com>
2023-06-08 18:15:24 -0400

    init commit

---
+++ entro.py
@@ -0,0 +1,17 @@
+"""
+entro.py
+6/8/23
+
+a text-based adventure game, terminal-based
+beginner project
+reference files in other files, python used
+in order to progress the story
+
+created by slugcat
+used for beginner/intermediate python
+developers to learn more about python
+"""
+
+#IMPORTS
+import random #will be necessary later
+#todo: the game
---
+++ readme.md
@@ -0,0 +1,12 @@
+# ENTRO.py
+
+entropy
+
+## how to play
+run entro.py
+
+## how to add/mod
+edit the data folder
+edit entro.py
+
+enjoy
